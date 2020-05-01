69a8164da4c7c8405b72863b1675558f76f6fa5e
Adam Gardner <adam@agardner.net>
2020-05-01 13:20:20 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-client = UnleashClient("http://unleash:4242/api", "My Program")
+client = UnleashClient(url="http://unleash:4242/api",app_name="My Program",refresh_interval=1)
 client.initialize_client()
 
 @app.route('/', methods=['GET'])
@@ -12,9 +12,10 @@ def do_homepage():
   statusCode = request.args.get('statusCode');
 
   if statusCode == "500":
-  	return 500;
+    return 500;
+
   if client.is_enabled("NormalDelivery", default_value=True):
     return current_app.send_static_file('index.html')
   else:
-    r = requests.get('https://raw.githubusercontent.com/agardnerIT/OddFiles/master/index2.html')
-    return r.text
+   r = requests.get('https://raw.githubusercontent.com/agardnerIT/OddFiles/master/index2.html')
+   return r.text
